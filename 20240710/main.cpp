25b3aed774f85f59535b9add7cd067730766ce19
unknown <phumsirisumativit@gmail.com>
2024-07-10 19:47:54 +0700

    add: 10072024

--- 20240710/main.cpp
+++ 20240710/main.cpp
@@ -3,9 +3,18 @@ using namespace std;
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        
+        int cnt=0;
+        for(string &x:logs){
+            if (x=="./") continue;
+            else if (cnt!=0 && x=="../") --cnt;
+            else if (x!="../") ++cnt;
+        }
+        return cnt;
     }
 };
 int main(){
-    
+    Solution s;
+    vector<string> l1={"d1/","d2/","./","d3/","../","d31/"},l2={"d1/","../","../","../"};
+    cout<<s.minOperations(l1)<<endl;
+    cout<<s.minOperations(l2)<<endl;
 }
