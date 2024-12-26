3e0ec2fc67f22c6b5cfcb67b43fdf2286de964d5
Phumsirii <phumsirisumativit@gmail.com>
2024-12-26 15:01:26 +0700

    edit: 26122024

--- 20241226/main.cpp
+++ 20241226/main.cpp
@@ -6,22 +6,28 @@ using namespace std;
 
 class Solution {
 public:
-    unordered_map<string, int> dp;
+    // Custom hash function for pair<int, int>
+    struct pair_hash {
+        template <typename T1, typename T2>
+        std::size_t operator()(const std::pair<T1, T2>& p) const {
+            return std::hash<T1>()(p.first) ^ std::hash<T2>()(p.second);
+        }
+    };
+
+    // index and currentSum -> the number of ways to reach the currentSum
+    unordered_map<pair<int,int>, int, pair_hash> dp;
 
     int findTargetSumWays(vector<int>& nums, int target) {
         return recur(nums, target, 0);
     }
 
-    int recur(vector<int>& nums, int target, int idx) {
+    int recur(vector<int>& nums, int currentSum, int idx) {
         if (idx == nums.size()) {
-            return target == 0;
+            return currentSum == 0;
         }
-        string key = to_string(idx) + "," + to_string(target);
-        if (dp.find(key) != dp.end()) {
-            return dp[key];
+        if (dp.find({idx, currentSum}) != dp.end()) {
+            return dp[{idx, currentSum}];
         }
-        int add = recur(nums, target - nums[idx], idx + 1);
-        int subtract = recur(nums, target + nums[idx], idx + 1);
-        return dp[key] = add + subtract;
+        return dp[{idx, currentSum}] = recur(nums, currentSum - nums[idx], idx + 1) + recur(nums, currentSum + nums[idx], idx + 1);
     }
 };
