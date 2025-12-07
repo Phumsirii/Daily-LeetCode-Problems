f0e3d681ceb620b1507bee6b13cc2b48b426a257
Phumsirii <phumsirisumativit@gmail.com>
2025-12-07 20:03:59 +0700

    add: 05122025

---
+++ 20251205/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int countPartitions(vector<int> &nums)
+    {
+        int sum = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+        }
+        if (sum % 2 != 0)
+            return 0;
+        return n - 1;
+    }
+};
