e35f4fe9ab527ad14adf23ce3f525de909b75ea5
Phumsirii <phumsirisumativit@gmail.com>
2025-12-07 19:48:24 +0700

    add: 07122025

---
+++ 20251207/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int countOdds(int low, int high)
+    {
+        if (low % 2 == 0)
+            ++low;
+        if (high % 2 == 0)
+            --high;
+        return (high - low) / 2 + 1;
+    }
+};
